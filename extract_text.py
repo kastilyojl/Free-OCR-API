545468d2dd658c6f4342a1a2aacf342438868dc9
kastilyojl <castillojlb0922@gmail.com>
2024-12-03 09:49:06 +0800

    intial commit

---
+++ extract_text.py
@@ -0,0 +1,45 @@
+import requests #pip install requests
+import json
+
+def ocr_space_file(filename, overlay=False, api_key='K84853722988957', language='eng', OCREngine=2):
+    """OCR.space API request with local file."""
+    payload = {
+        'isOverlayRequired': overlay,
+        'apikey': api_key,
+        'language': language,
+        'OCREngine': OCREngine,
+    }
+    with open(filename, 'rb') as f:
+        print("Sending request to OCR API...")
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          timeout=10)
+
+    if r.status_code != 200:
+        print(f"Error: {r.status_code} - {r.text}")
+        return None
+
+    return r.content.decode()
+
+if __name__ == "__main__":
+
+    response = ocr_space_file(filename=r"lorem-ipsum.jpg",
+                              language='eng',
+                              OCREngine=2)
+
+    if response:
+        print("Response:", response)
+
+        try:
+            json_response = json.loads(response)
+            parsed_results = json_response.get("ParsedResults", [])
+
+            if parsed_results:
+                extracted_text = parsed_results[0].get("ParsedText", "")
+                print("\nExtracted Text:\n\n", extracted_text) # Display the extracted text in console
+
+            else:
+                print("No text found.")
+        except json.JSONDecodeError as e:
+            print("Error parsing JSON response:", e)
